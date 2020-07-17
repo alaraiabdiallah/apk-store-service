12c659ff36e2340147c0c9d72670f833308a06e2
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2020-07-17 09:19:56 +0700

    message error apikey missing

--- protocols/http/middlewares/common_middleware.go
+++ protocols/http/middlewares/common_middleware.go
@@ -12,7 +12,7 @@ func APIAuth(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		api_key := c.Request().Header.Get("apikey")
 		if api_key != os.Getenv("API_KEY") {
-			err := errors.New("x-api-key invalid")
+			err := errors.New("apikey invalid")
 			return c.JSON(http.StatusForbidden, helpers.FailedJsonMessage(err.Error()))
 		}
 		return next(c)
